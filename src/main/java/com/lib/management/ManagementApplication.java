b5424e34febc65fa22a610f03e4425aa642f2ea4
zhangyonglele <zhangyonglele@yeah.net>
2020-03-01 21:46:33 +0800

    建立项目结构，编写部分配置文件

--- src/main/java/com/lib/management/ManagementApplication.java
+++ src/main/java/com/lib/management/ManagementApplication.java
@@ -1,9 +1,13 @@
 package com.lib.management;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("/com.lib.management.mapper")
 public class ManagementApplication {
 
     public static void main(String[] args) {
