5e55b238bf7ea37eda954798b99a0833c2bef099
sky00000000 <1136188281@qq.com>
2020-04-03 13:47:36 +0800

    增加超管登录登出和修改密码功能

--- src/main/java/com/lib/management/controller/SuperAdminController.java
+++ src/main/java/com/lib/management/controller/SuperAdminController.java
@@ -4,19 +4,56 @@ import com.lib.management.filter.annotation.LoginRequire;
 import com.lib.management.model.SuperAdmin;
 import com.lib.management.service.SuperAdminService;
 import com.lib.management.util.UniversalResponseBody;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
-
+@Slf4j
 @RestController
-@CrossOrigin
+@RequestMapping("/admin")
 public class SuperAdminController {
     @Resource
     private SuperAdminService superAdminService;
 
+    @GetMapping("/login")
+    public UniversalResponseBody superAdminLogin(String name,String password, HttpSession session){
+        SuperAdmin admin = superAdminService.searchAccountByAdminName(name);
+        if(admin != null && admin.getSuperAdminPassword().equals(password)){
+            session.setAttribute("auth","superAdmin");
+            session.setAttribute("adminInfo",admin);
+            return new UniversalResponseBody(0,"success");
+        }else {
+            log.warn("[WARRING CHECK] userName "+name+",password " +password);
+            return new UniversalResponseBody(-1,"error");
+        }
+    }
+    @GetMapping("/logout")
+    @LoginRequire("superAdmin")
+    public UniversalResponseBody superAdminLogout(HttpSession session){
+        session.invalidate();
+        return new UniversalResponseBody(0,"success");
+    }
+
+    @PutMapping("/password")
+    @LoginRequire("superAdmin")
+    public UniversalResponseBody updatePassword(String name,String oldPwd, String newPwd, String newPwdRep,
+    HttpSession session){
+        if(!newPwd.equals(newPwdRep)){
+            log.warn("[WARRING CHECK] newPassword "+newPwd + " newPasswordREP " +newPwdRep);
+            return new UniversalResponseBody(-1,"error");
+        }
+        SuperAdmin superAdmin = superAdminService.searchAccountByAdminName(name);
+        if(superAdmin != null && superAdmin.getSuperAdminPassword().equals(oldPwd)){
+            superAdminService.updateAccountByAdminName(name,newPwd);
+            return new UniversalResponseBody(0,"success");
+        }else {
+            log.warn("[WARRING CHECK] userName "+name+",password " +oldPwd);
+            return new UniversalResponseBody(-1,"error");
+        }
+
+
+    }
     @GetMapping("/admin/info")
     public UniversalResponseBody getSuperAdminInfoByName(String name,HttpSession session){
         //在RestController后，方法返回的对象将会被自动解析成json数据
